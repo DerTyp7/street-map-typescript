42043fd7b549b390ce60841c6cd6b0dea9fbf298
Janis Meister <meister.nets@gmail.com>
2022-01-14 13:25:46 +0100

    generated osrm service

---
+++ src/app/osrm.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=full&steps=true&geometries=geojson
+export class OsrmService {
+
+  constructor() { }
+}
